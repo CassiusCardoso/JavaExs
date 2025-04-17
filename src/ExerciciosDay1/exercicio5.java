981ed99d9eeafaffd7f4ae13931acc41294eda3c
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-04-17 10:18:02 -0300

    feat: Corrigindo problemas na lógica do menu interativo

--- src/ExerciciosDay1/exercicio5.java
+++ src/ExerciciosDay1/exercicio5.java
@@ -15,31 +15,58 @@ Use um menu com switch para as opções:
          */
         Scanner sc = new Scanner(System.in);
 
-        // Perguntando ao usuário qual produto quer
-        System.out.println("Qual produto deseja adicionar à lista: ");
-        String item = sc.nextLine();
-
+        // Criando a array lista
         ArrayList<String> lista = new ArrayList<String>();
-        // Menu com o switch como se fosse um while em Python
-        System.out.println("1 - Adicionar / 2 - Remover / 3 - Mostrar / 4 - Sair");
-        int resposta = sc.nextInt();
+
+        // Variável de condição para controlar o loop while
         boolean executando = true;
+
         while (executando){
+            // Menu com o switch como se fosse um while em Python
+            // A pergunta precisa ficar dentro do loop logo no começo, antes dos cases. Isso para perguntar mais de uma vez
+            System.out.println("1 - Adicionar / 2 - Remover / 3 - Mostrar / 4 - Sair");
+            int resposta = sc.nextInt();
+            sc.nextLine(); // Limpar o ENTER pendente
             switch(resposta){
                 case 1:
+                    // Perguntando ao usuário qual produto quer
                     System.out.println("Digite o nome do produto que deseja adicionar à lista:");
+                    String item = sc.nextLine();
                     lista.add(item);
+                    System.out.println("Item digitado com sucesso!");
                     break;
                 case 2:
-                    lista.remove(item);
+
+                    // Lista ao usuário os itens que existem na lista
+                    System.out.println("Itens disponíveis para remover: " + lista);
+                    // Captura a resposta do usuário para verificar o item
+                    System.out.println("Qual o item que você deseja remover: ");
+                    String itemRemovido = sc.nextLine();
+
+                    // IF para verificar se a resposta que o usuário digitou existe na lista
+                    if (lista.contains(itemRemovido)){
+                        lista.remove(itemRemovido);
+                        System.out.println("Item removidado com sucesso!");
+                    }
+
                     break;
-                case 3:
-                    lista.get(1);
+                case 3: // Case que mostra todos os itens da lista
+
+                    StringBuilder resultado = new StringBuilder();
+                    resultado.append("Itens da lista: \n");
+
+                    // ArrayList tem o método .size() para retonar o tamanho e não .length()
+                    for (int i = 0; i < lista.size(); i++){
+                        resultado.append("- " + lista.get(i)).append("\n");
+                    }
+                    System.out.println(resultado.toString());
                     break;
                 case 4:
                     executando = false;
+                    System.out.println("Programa finalizado com sucesso!");
                     break;
         }
     }
+        sc.close();
     }
 }
